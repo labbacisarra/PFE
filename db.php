7c9ea4c4bf6d3efc921f27e1cf9853394b49a03c
labbacisarra <Ayoubiayoub018@gmail.com>
2026-05-31 03:33:48 +0100

    Change database connection and enable error reporting
    Updated database connection settings and enabled error reporting.

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=ecolna;charset=utf8",
-    "root",
-    ""
-);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try {
+    $pdo = new PDO("mysql:host=sql303.byetcluster.com;dbname=if0_42043747_ecolna;charset=utf8", "if0_42043747", "ywfpzzUywmUo7I");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "خطأ في الاتصال: " . $e->getMessage();
+}
 ?>
